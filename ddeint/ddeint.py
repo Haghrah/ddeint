10e7a26e196a021780688dabfeca6c39ec82900a
Amir Arslan Haghrah <7789561+Haghrah@users.noreply.github.com>
2019-06-25 21:08:51 +0430

    New version of ddeint
    New version of ddeint for multi variable systems with multiple time delay values.

--- ddeint/ddeint.py
+++ ddeint/ddeint.py
@@ -1,12 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 """
 This module implements ddeint, a simple Differential Delay Equation
-solver built on top of Scipy's odeint """
-
+solver built on top of Scipy's odeint"""
 # REQUIRES Numpy and Scipy.
 import numpy as np
 import scipy.integrate
 import scipy.interpolate
-
 
 class ddeVar:
     """
@@ -28,19 +28,33 @@ class ddeVar:
             kind='linear', bounds_error=False,
             fill_value = self.g(tc))
 
-    def update(self,t,Y):
+    def update(self, t, Y):
         """ Add one new (ti,yi) to the interpolator """
-
         self.itpr.x = np.hstack([self.itpr.x, [t]])
-        Y2 = Y if (Y.size==1) else np.array([Y]).T
-        self.itpr.y = np.hstack([self.itpr.y, Y2])
+        self.itpr.y = np.hstack([self.itpr.y, Y])
         self.itpr.fill_value = Y
 
     def __call__(self,t=0):
         """ Y(t) will return the instance's value at time t """
+        return (self.g(t) if (t<self.tc) else self.itpr(t))
 
-        return (self.g(t) if (t<=self.tc) else self.itpr(t))
 
+class ddeVars:
+    
+    def __init__(self, gs, tc=0):
+        self.tc= tc
+        self.n = len(gs)
+        self.Vars = [ddeVar(g, tc) for g in gs]
+    
+    def update(self, t, Y):
+        for i in range(self.n):
+            self.Vars[i].update(t, Y[i])
+    
+    def __getitem__(self, key):
+        return self.Vars[key]
+        
+    def __call__(self, t=0):
+        return np.array([var(t) for var in self.Vars])
 
 
 class dde(scipy.integrate.ode):
@@ -50,60 +64,51 @@ class dde(scipy.integrate.ode):
     integration step.
     """
 
-    def __init__(self,f,jac=None):
+    def __init__(self, f):
 
         def f2(t,y,args):
-            return f(self.Y,t,*args)
-        scipy.integrate.ode.__init__(self,f2,jac)
+            return f(self.Y, t, *args)
+        scipy.integrate.ode.__init__(self, f2)
         self.set_f_params(None)
 
     def integrate(self, t, step=0, relax=0):
-
-        scipy.integrate.ode.integrate(self,t,step,relax)
-        self.Y.update(self.t,self.y)
+        scipy.integrate.ode.integrate(self, t, step, relax)
+        self.Y.update(self.t, self.y)
         return self.y
 
-    def set_initial_value(self,Y):
-
+    def set_initial_value(self, Y):
         self.Y = Y #!!! Y will be modified during integration
         scipy.integrate.ode.set_initial_value(self, Y(Y.tc), Y.tc)
 
 
-
-def ddeint(func,g,tt,fargs=None):
+def ddeint(func, gs, tt, fargs=None):
     """ Solves Delay Differential Equations
-
     Similar to scipy.integrate.odeint. Solves a Delay differential
     Equation system (DDE) defined by
-
         Y(t) = g(t) for t<0
         Y'(t) = func(Y,t) for t>= 0
-
     Where func can involve past values of Y, like Y(t-d).
     
-
     Parameters
     -----------
     
     func
       a function Y,t,args -> Y'(t), where args is optional.
-      The variable Y is an instance of class ddeVar, which means that
-      it is called like a function: Y(t), Y(t-d), etc. Y(t) returns
-      either a number or a numpy array (for multivariate systems).
-
-    g
-      The 'history function'. A function g(t)=Y(t) for t<0, g(t)
-      returns either a number or a numpy array (for multivariate
-      systems).
+      The variable Y is an instance of class ddeVars (A class containing 
+      a list of ddeVar objects). It's elements are called like a 
+      function: Y[0](t), Y[0](t-d), etc. Each element of Y represents a 
+      state variable which can have a different delay value. It must be 
+      noticed that, Y[i](t) returns a number.
+      
+    gs
+      The 'list of history functions'. A list of functions g(t) = Y[i](t) 
+      for t<0, g(t) returns a number.
     
     tt
       The vector of times [t0, t1, ...] at which the system must
       be solved.
-
     fargs
       Additional arguments to be passed to parameter ``func``, if any.
-
-
     Examples
     ---------
     
@@ -118,24 +123,28 @@ def ddeint(func,g,tt,fargs=None):
         dy/dt = -0.5* ( 1- x(t-d) )
     
     The delay ``d`` is a tunable parameter of the model.
-
     >>> import numpy as np
     >>> from ddeint import ddeint
     >>> 
-    >>> def model(XY,t,d):
-    >>>     x, y = XY(t)
-    >>>     xd, yd = XY(t-d)
-    >>>     return np.array([0.5*x*(1-yd), -0.5*y*(1-xd)])
+    >>> def model(X, t, d):
+    >>>     x = X[0](t)
+    >>>     xd = X[0](t - d)
+    >>>     y = X[1](t)
+    >>>     yd = X[1](t - d)
+    >>>     return array([0.5 * x * (1 - yd), -0.5 * y * (1 - xd)])
     >>> 
-    >>> g = lambda t : np.array([1+t,2-t]) # 'history' at t<0
-    >>> tt = np.linspace(0,30,20000) # times for integration
+    >>> g1 = lambda t: 1 # 'history' at t<0 for x
+    >>> g2 = lambda t: 2 # 'history' at t<0 for y
+    >>> tt = np.linspace(0, 30, 20000) # times for integration
     >>> d = 0.5 # set parameter d 
-    >>> yy = ddeint(model,g,tt,fargs=(d,)) # solve the DDE !
+    >>> yy = ddeint(model, [g1, g2], tt, fargs=(d,)) # solve the DDE !
      
     """
 
     dde_ = dde(func)
-    dde_.set_initial_value(ddeVar(g,tt[0]))
+    dde_.set_initial_value(ddeVars(gs,tt[0]))
     dde_.set_f_params(fargs if fargs else [])
-    results = [dde_.integrate(dde_.t + dt) for dt in np.diff(tt)]
-    return np.array( [g(tt[0])] + results)
+    results = [np.array([g(tt[0]) for g in gs])]
+    results.extend(dde_.integrate(dde_.t + dt) for dt in np.diff(tt))
+    return np.array(results)
+
